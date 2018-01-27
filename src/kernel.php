6a071723c9ad4e21ddbd10c5511f7f45f407173e
SenaYan <qinuoyun@qq.com>
2018-01-27 22:03:56 +0800

    no message

---
+++ src/kernel.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Conveyancer PHP Framework
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright 2016-2018 Yan TianZeng<qinuoyun@qq.com>
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT
+ * @link      http://www.ub-7.com
+ */
+namespace conveyancer\framework;
+/**
+ * 框架基础配置文件
+ */
+class kernel {
+
+    public function __construct() {
+        # code...
+    }
+
+    public function test() {
+        echo "framework-测试" . "<br>";
+    }
+
+}
